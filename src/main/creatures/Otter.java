793552cf86afb6480496588caaa1993f434a1a9b
DavidInsig <davidinsig7@gmail.com>
2022-05-11 08:24:14 -0400

    yea

---
+++ src/main/creatures/Otter.java
@@ -0,0 +1,8 @@
+package creatures;
+
+public class Otter extends AquaticMammal{
+
+    public void method2() {System.out.print("Paws");}
+
+    public String toString() {return ("beach dweller");}
+}
